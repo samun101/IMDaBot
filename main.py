be5f5cc45bc9c63c602bcbc91b1ad9b3d3e5a2ea
samundson101 <samundson@westmont.edu>
2022-04-30 23:46:01 -0400

    adding the gitignore and ignoring certain files that are unnecessary

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import discord
 import os
 from discord.ext import commands
+import json
 
-vClient = commands.Bot(command_prefix="?")
+if os.path.exists(os.getcwd()+"/config.json"):
+    with open("./config.json") as f:
+        configData = json.load(f)
+else:
+    configTemplate={"Token":"","Prefix":"?"}
+    with open(os.getcwd()+"/config.json", "w+")as f:
+        json.dump(configTemplate,f)
 
+token = configData["Token"]
+prefix = configData["Prefix"]
+
+vClient = commands.Bot(command_prefix=prefix)
 @vClient.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(vClient))
@@ -17,4 +28,4 @@ if __name__ == '__main__':
     for ext in inc_ext:
         vClient.load_extension(ext)
 
-vClient.run('OTYwNjM1NzM5NTUxNDU3MzEx.YktTwA.oBwD9eLcuzIW4UMdKCIIliyYXO0')
+vClient.run(token)
